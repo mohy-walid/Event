5cfad8eda97cd2e092d2ec81038ddb524b07865c
mohy-walid <mohy.walid86@gmail.com>
2025-09-02 04:10:32 +0300

    feat(navbar): highlight active link based on current page

--- js/index.js
+++ js/index.js
@@ -1,10 +1,29 @@
+
 function loadComponent(id, file) {
   fetch(file)
     .then(res => res.text())
     .then(data => {
       document.getElementById(id).innerHTML = data;
+
+  
+      setActiveNavLink();
     });
 }
+
+function setActiveNavLink() {
+  const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+  const currentPath = window.location.pathname.split('/').pop();
+
+  navLinks.forEach(link => {
+    const linkPath = link.getAttribute('href').split('/').pop();
+    if (linkPath === currentPath) {
+      link.classList.add('active');
+    } else {
+      link.classList.remove('active');
+    }
+  });
+}
+
 document.querySelectorAll(".visit-btn").forEach(function(btn){
   btn.addEventListener("click", function(){
     window.location.href = "html/details.html";
