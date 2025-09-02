1ae2525c039bedf946770f233f6b64faa73b9a59
mohy-walid <mohy.walid86@gmail.com>
2025-09-02 13:27:28 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,28 +1,28 @@
-
 function loadComponent(id, file) {
   fetch(file)
     .then(res => res.text())
     .then(data => {
       document.getElementById(id).innerHTML = data;
+
+      // استدعي تحديد اللينك النشط بعد تحميل النافبار
+      setActiveNavLink();
     });
 }
 
-document.addEventListener("DOMContentLoaded", function () {
+function setActiveNavLink() {
   const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
   let currentPath = window.location.pathname;
 
- 
+  // هات آخر جزء من الرابط (اسم الملف)
   currentPath = currentPath.substring(currentPath.lastIndexOf("/") + 1);
 
- 
+  // لو الصفحة الرئيسية
   if (currentPath === "" || currentPath === "/") {
     currentPath = "index.html";
   }
 
   navLinks.forEach(link => {
     let linkPath = link.getAttribute("href");
-
-    
     linkPath = linkPath.substring(linkPath.lastIndexOf("/") + 1);
 
     if (linkPath === currentPath) {
@@ -31,7 +31,8 @@ document.addEventListener("DOMContentLoaded", function () {
       link.classList.remove("active");
     }
   });
-});
+}
+
 
 
 document.querySelectorAll(".visit-btn").forEach(function(btn){
